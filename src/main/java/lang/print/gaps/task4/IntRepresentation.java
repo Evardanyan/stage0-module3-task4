619faa71b97270212c0c1bf9455b852ae218835c
Edgar Vardanyan <e.vardan4an@gmail.com>
2022-06-25 23:41:11 +0400

    test_v1

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,14 +2,11 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        char c = 'c';
-        char h = 'h';
-        char a = 'a';
-        char r = 'r';
-        int cNum = c;
-        int hNum = h;
-        int aNum = a;
-        int rNum = r;
-        System.out.println(cNum + "\n" + hNum + "\n" + aNum + "\n" + rNum);
+        char c = 99;
+        char h = 104;
+        char a = 97;
+        char r = 114;
+
+        System.out.println(c + "\n" + h + "\n" + a + "\n" + r);
     }
 }
